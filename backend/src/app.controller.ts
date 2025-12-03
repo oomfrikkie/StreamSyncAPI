bac9f7543b2075fdcf452dc41a7ee7c5d4b2e0a5
Iarina Dicu <iarina.dicu@student.nhlstenden.com>
2025-12-03 16:06:02 +0100

    Update app.controller.ts with new endpoints

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -5,13 +5,10 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-@Get()
-home() {
-  const members = this.appService.getMembers();
-  return `${members} NestJS + Postgres is working!`;
-}
-
-
+  @Get()
+  home() {
+    return 'NestJS + Postgres is working!';
+  }
 
   @Get('members')
   getMembers() {
@@ -21,5 +18,45 @@ home() {
   @Get('qualities')
   getQualities() {
     return this.appService.getQualities();
+  }
+
+  @Get('api-user-accounts')
+  getApiUserAccounts() {
+    return this.appService.getApiUserAccounts();
+  }
+
+  @Get('accounts')
+  getAccounts() {
+    return this.appService.getAccounts();
+  }
+
+  @Get('account-subscriptions')
+  getAccountSubscriptions() {
+    return this.appService.getAccountSubscriptions();
+  }
+
+  @Get('invitations')
+  getInvitations() {
+    return this.appService.getInvitations();
+  }
+
+  @Get('profiles')
+  getProfiles() {
+    return this.appService.getProfiles();
+  }
+
+  @Get('age-categories')
+  getAgeCategories() {
+    return this.appService.getAgeCategories();
+  }
+
+  @Get('profile-genre-preferences')
+  getProfileGenrePreferences() {
+    return this.appService.getProfileGenrePreferences();
+  }
+
+  @Get('genres')
+  getGenres() {
+    return this.appService.getGenres();
   }
 }
