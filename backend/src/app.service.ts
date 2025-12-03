9c5a87ca94449b73aaacef552bebfe1b702d499b
Iarina Dicu <iarina.dicu@student.nhlstenden.com>
2025-12-03 16:07:00 +0100

    Add service methods for ERD tables

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -14,4 +14,44 @@ export class AppService {
     const result = await this.dataSource.query('SELECT * FROM "quality"');
     return result;
   }
+
+  async getApiUserAccounts() {
+    const result = await this.dataSource.query('SELECT * FROM "api_user_account"');
+    return result;
+  }
+
+  async getAccounts() {
+    const result = await this.dataSource.query('SELECT * FROM "account"');
+    return result;
+  }
+
+  async getAccountSubscriptions() {
+    const result = await this.dataSource.query('SELECT * FROM "account_subscription"');
+    return result;
+  }
+
+  async getInvitations() {
+    const result = await this.dataSource.query('SELECT * FROM "invitation"');
+    return result;
+  }
+
+  async getProfiles() {
+    const result = await this.dataSource.query('SELECT * FROM "profile"');
+    return result;
+  }
+
+  async getAgeCategories() {
+    const result = await this.dataSource.query('SELECT * FROM "age_category"');
+    return result;
+  }
+
+  async getProfileGenrePreferences() {
+    const result = await this.dataSource.query('SELECT * FROM "profile_genre_preference"');
+    return result;
+  }
+
+  async getGenres() {
+    const result = await this.dataSource.query('SELECT * FROM "genre"');
+    return result;
+  }
 }
