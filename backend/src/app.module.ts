587ef07782dbe5553034e08fffd21455c09930d9
Derjen Frick <derjenyt@gmail.com>
2025-12-21 20:26:59 +0100

    FIxed container

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -22,16 +22,17 @@ import { WatchlistModule } from './watchlist/watchlist.module';
     }),
 
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DB_HOST,
-      port: parseInt(process.env.POSTGRES_PORT!, 10),
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      autoLoadEntities: true,
-      synchronize: false,
-      logging: ['query', 'error'],
-    }),
+  type: 'postgres',
+  host: process.env.POSTGRES_HOST,
+  port: parseInt(process.env.POSTGRES_PORT!, 10),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+  autoLoadEntities: true,
+  synchronize: false,
+  logging: ['query', 'error'],
+}),
+
 
     AccountModule,
     AccountTokenModule,
