2dd7fab7b5709fafde7fa636c2a49a49dfcda04d
Iarina Dicu <iarina.dicu@student.nhlstenden.com>
2025-12-23 14:15:34 +0100

    Update ProfileSerivce to handle genres and min quality

--- backend/src/profile/profile.service.ts
+++ backend/src/profile/profile.service.ts
@@ -1,10 +1,8 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
-
+import { Repository, In } from 'typeorm';
 import { Profile } from './profile.entity';
-import { Account } from '../account/account.entity';
-import { AgeCategory } from '../age-category/age-category.entity';
+import { Genre } from '../content/genre/genre.entity';
 import { CreateProfileDto } from './dto-profile/create-profile.dto';
 
 @Injectable()
@@ -13,73 +11,30 @@ export class ProfileService {
     @InjectRepository(Profile)
     private readonly profileRepo: Repository<Profile>,
 
-    @InjectRepository(Account)
-    private readonly accountRepo: Repository<Account>,
-
-    @InjectRepository(AgeCategory)
-    private readonly ageRepo: Repository<AgeCategory>,
+    @InjectRepository(Genre)
+    private readonly genreRepo: Repository<Genre>,
   ) {}
 
   async create(dto: CreateProfileDto) {
-    // Make sure account exists
-    const account = await this.accountRepo.findOne({
-      where: { account_id: dto.account_id },
-    });
+    const genres = dto.preferredGenres
+      ? await this.genreRepo.find({
+          where: { genreId: In(dto.preferredGenres) },
+        })
+      : [];
 
-    if (!account) {
-      throw new NotFoundException('Account not found');
-    }
-
-    // Make sure age category exists
-    const ageCategory = await this.ageRepo.findOne({
-      where: { age_category_id: dto.age_category_id },
-    });
-
-    if (!ageCategory) {
-      throw new NotFoundException('Age category not found');
-    }
-
-    // Create profile object
-    const newProfile = this.profileRepo.create({
+    const profile = this.profileRepo.create({
+      account_id: dto.account_id,
+      age_category_id: dto.age_category_id,
       name: dto.name,
       image_url: dto.image_url ?? null,
-      account: account,
-      age_category: ageCategory, // EXACT NAME FROM ENTITY
+      preferredGenres: genres,
+      minQuality: dto.minQuality ?? 'SD',
     });
 
-    const saved = await this.profileRepo.save(newProfile);
-
-return {
-  profile_id: saved.profile_id,
-  name: saved.name,
-  image_url: saved.image_url,
-  age_category: {
-    age_category_id: ageCategory.age_category_id,
-    name: ageCategory.name,
-  },
-};
-
+    return this.profileRepo.save(profile);
   }
-
 
   async getAllProfiles() {
-    return this.profileRepo.find({
-      relations: ['account', 'age_category'],
-    });
+    return this.profileRepo.find();
   }
-
-  
-async delete(profileId: number) {
-  const profile = await this.profileRepo.findOne({
-    where: { profile_id: profileId },
-  });
-
-  if (!profile) {
-    throw new NotFoundException('Profile not found');
-  }
-
-  await this.profileRepo.remove(profile);
-  return { message: 'Profile deleted' };
-}
-
 }
